e8bb09f2a66d116ba2826bc5224f3660496c2e2d
Lizzie Chan <lchan217@gmail.com>
2019-03-27 15:37:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,8 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'pry'
 class InteractiveRecord
-    def find_by(row)
-      binding.pry
+    def self.find_by(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+      DB[:conn].execute(sql)
   end
 end
